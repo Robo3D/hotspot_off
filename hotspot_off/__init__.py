54dd5269e1aac1c1670453191f904d190b89ec62
Victor E <victorevector@gmail.com>
2017-06-02 13:43:35 -0700

    fix?

--- hotspot_off/__init__.py
+++ hotspot_off/__init__.py
@@ -1,20 +1,10 @@
 # coding=utf-8
 from __future__ import absolute_import
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
+import subprocess
 
-class Hotspot_offPlugin(octoprint.plugin.SettingsPlugin,
-                        octoprint.plugin.AssetPlugin,
-                        octoprint.plugin.TemplatePlugin,
-                        octoprint.plugin.StartupPlugin):
+class Hotspot_offPlugin(octoprint.plugin.SettingsPlugin, octoprint.plugin.AssetPlugin, octoprint.plugin.TemplatePlugin, octoprint.plugin.StartupPlugin):
+
     def on_startup(self, host, port):
         args = ['netconnectcli', 'stop_ap']
         try:
@@ -24,45 +14,20 @@ class Hotspot_offPlugin(octoprint.plugin.SettingsPlugin,
         finally:
             self._logger.info('### Stop Hotspot Results: {}'.format(r))
 
-    ##~~ SettingsPlugin mixin
-    def get_settings_defaults(self):
-        return dict(
-            # put your plugin's default settings here
-        )
-
-    ##~~ AssetPlugin mixin
-
-    def get_assets(self):
-        # Define your plugin's asset files to automatically include in the
-        # core UI here.
-        return dict(
-            js=["js/hotspot_off.js"],
-            css=["css/hotspot_off.css"],
-            less=["less/hotspot_off.less"]
-        )
-
-    ##~~ Softwareupdate hook
     def get_update_information(self):
         return dict(
                 hotspot_off=dict(
                 displayName="Hotspot_off Plugin",
                 displayVersion=self._plugin_version,
-
-                # version check: github repository
                 type="github_release",
                 user="Robo3D",
                 repo="hotspot_off",
                 current=self._plugin_version,
-
-                # update method: pip
                 pip="https://github.com/Robo3D/hotspot_off/archive/{target_version}.zip"
             )
         )
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Hotspot_off Plugin"
 
 def __plugin_load__():
